be496fe8086fbc94dc59ccbbee37ba026d2ed335
Your Name <you@example.com>
2023-01-16 22:59:43 +0800

    add loader dir,  load loader_func to address 0x8000

--- source/boot/boot.c
+++ source/boot/boot.c
@@ -5,17 +5,19 @@
  * boot扇区容量较小，仅512字节。由于dbr占用了不少字节，导致其没有多少空间放代码，
  * 所以功能只能最简化,并且要开启最大的优化-os
  *
- * 作者：李述铜
- * 联系邮箱: 527676163@qq.com
  */
 __asm__(".code16gcc");
 
 #include "boot.h"
 
+#define LOADER_ADDR 0X8000
+
 /**
  * Boot的C入口函数
  * 只完成一项功能，即从磁盘找到loader文件然后加载到内容中，并跳转过去
  */
-void boot_entry(void) {
+void boot_entry(void) 
+{
+    ((void(*)(void))LOADER_ADDR)();
 }
 
