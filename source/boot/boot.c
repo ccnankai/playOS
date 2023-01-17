8646941684804afaaddd29cb68e5c156f946a63f
Your Name <you@example.com>
2023-01-17 10:42:49 +0800

    add show_msg function,can display custom string

--- source/boot/boot.c
+++ source/boot/boot.c
@@ -10,14 +10,13 @@ __asm__(".code16gcc");
 
 #include "boot.h"
 
-#define LOADER_ADDR 0X8000
+#define	LOADER_START_ADDR	0x8000		// loader加载的地址
 
 /**
  * Boot的C入口函数
  * 只完成一项功能，即从磁盘找到loader文件然后加载到内容中，并跳转过去
  */
-void boot_entry(void) 
-{
-    ((void(*)(void))LOADER_ADDR)();
-}
+void boot_entry(void) {
+	((void (*)(void))LOADER_START_ADDR)();
+} 
 
--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -1,7 +1,25 @@
+
+// 16位代码，必须加上放在开头，以便有些io指令生成为32位
 __asm__(".code16gcc");
 
-void loader_entry(void)
-{
-    for(;;){}
+/**
+ * BIOS下显示字符串
+ */
+static void show_msg (const char * msg) {
+    char c;
 
+	// 使用bios写显存，持续往下写
+	while ((c = *msg++) != '\0') {
+		__asm__ __volatile__(
+				"mov $0xe, %%ah\n\t"
+				"mov %[ch], %%al\n\t"
+				"int $0x10"::[ch]"r"(c));
+	}
 }
+
+void loader_entry(void) {
+    show_msg("....loading.....\r\n");
+    for(;;) {}
+}
+
+
